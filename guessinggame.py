4c8cb604fe32d91bc5ccb34f7dda79f9b131bb99
christianmcay <73971223+christianmcay@users.noreply.github.com>
2023-07-10 17:42:41 -0400

    guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -7,7 +7,7 @@ print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1, 20)
-print('Well, ' + myName + ', I ame thinking of a number between 1 and 20.')
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 for guessesTaken in range(6):
     print('Take a guess.')
